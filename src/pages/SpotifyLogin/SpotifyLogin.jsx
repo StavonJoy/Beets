418aee0f41fdc616767ed3501393fd708c9f3192
kkonkel23 <kailahkonkel@gmail.com>
2020-09-22 12:43:25 -0400

    style non-user pages

--- src/pages/SpotifyLogin/SpotifyLogin.jsx
+++ src/pages/SpotifyLogin/SpotifyLogin.jsx
@@ -4,9 +4,13 @@ import './SpotifyLogin.css'
 function SpotifyLogin() {
     return (
         <>
-            <h1>One more step!</h1>
-        <a href="http://localhost:3001/loginSpotify">Login to Spotify
-        </a>  
+            <div className="spotify-login">
+                <h1>One more step!</h1>
+                <a href="http://localhost:3001/loginSpotify">Login to Spotify</a>
+                <p>We appreciate your patience, to compensate, here's an adorable Beet illustraion...</p>
+                <img className="beets-pic" src="https://i.imgur.com/p8KFfHN.png" alt=""></img>
+                <p className="beets-text">Designed by Stavon Boavida</p>
+            </div>
 
         </>
     )
