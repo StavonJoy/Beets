091a7f57fad56a36ec84afe175aff90fd61fc9e2
Stavon Ewart <stavon@Stavons-MacBook-Air.local>
2020-09-23 15:55:10 -0400

    Adds my profile page and edit profile page

--- controllers/users.js
+++ controllers/users.js
@@ -2,9 +2,21 @@ const User = require("../models/user");
 
 module.exports = {
   index,
+  showProfile,
+  update
 };
+
+function showProfile(req, res){
+  User.findById(req.user._id)
+  .then((users) => res.json(users))
+}
 
 function index(req, res) {
   console.log("req.user", req.user);
   User.find({}).then((users) => res.json(users));
+}
+
+function update(req, res){
+  User.findByIdAndUpdate(req.user._id, req.body, {new: true})
+  .then((user)=> res.json(user))
 }
--- src/services/userService.js
+++ src/services/userService.js
@@ -10,3 +10,11 @@ export function getAllUsers() {
     { mode: "cors" }
   ).then((res) => res.json());
 }
+export function update(user) {
+  return fetch(`${BASE_URL}${user._id}`, {
+      method: "PUT",
+      headers: {'content-type': 'application/json', 'Authorization': 'Bearer ' + tokenService.getToken()},
+      body: JSON.stringify(user)
+  }, {mode: "cors"})
+  .then(res => res.json());
+}
