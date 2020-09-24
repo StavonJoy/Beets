7e22e410291558ccfa055aa270c007014c1d74e7
Cory Spicer <corys4139@gmail.com>
2020-09-24 14:23:23 -0400

    beet pic 2.0

--- src/pages/SpotifyLogin/SpotifyLogin.jsx
+++ src/pages/SpotifyLogin/SpotifyLogin.jsx
@@ -8,7 +8,7 @@ function SpotifyLogin() {
                 <h1>One more step!</h1>
                 <a href="http://localhost:3001/loginSpotify">Login to Spotify</a>
                 <p>We appreciate your patience, to compensate, here's an adorable Beet illustraion...</p>
-                <img className="beets-pic" src="https://i.imgur.com/p8KFfHN.png" alt=""></img>
+                <img className="beets-pic" src="https://i.imgur.com/JAaivc8.png" alt=""></img>
                 <p className="beets-text">Designed by Stavon Boavida</p>
             </div>
 
