2fa7c75cb71c4e25855f1c33e4105c5871f12f27
StavonJoy <67701866+StavonJoy@users.noreply.github.com>
2020-09-25 11:48:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        res.redirect('http://localhost:3000/playlists/#' +
+        res.redirect('https://beets-base.herokuapp.com/playlists/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
